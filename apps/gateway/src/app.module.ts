c21d08d43032e8bee30981833d8b5d85fc84fc6f
Cleber Gomes <cleber.gomes@govoll.com>
2021-10-22 23:28:22 -0300

    Initial commit

---
+++ apps/gateway/src/app.module.ts
@@ -0,0 +1,54 @@
+import { Module } from '@nestjs/common';
+import { GATEWAY_BUILD_SERVICE, GraphQLGatewayModule } from '@nestjs/graphql';
+import { RemoteGraphQLDataSource } from '@apollo/gateway';
+// import { decode } from 'jsonwebtoken';
+
+class AuthenticatedDataSource extends RemoteGraphQLDataSource {
+  async willSendRequest({ request, context }) {
+    // const { userId } = await decode(context.jwt);
+    // request.http.headers.set('x-user-id', userId);
+    request.http.headers.set('authorization', context.jwt);
+  }
+}
+
+@Module({
+  providers: [
+    {
+      provide: AuthenticatedDataSource,
+      useValue: AuthenticatedDataSource,
+    },
+    {
+      provide: GATEWAY_BUILD_SERVICE,
+      useFactory: (AuthenticatedDataSource) => {
+        return ({ name, url }) => new AuthenticatedDataSource({ url });
+      },
+      inject: [AuthenticatedDataSource],
+    },
+  ],
+  exports: [GATEWAY_BUILD_SERVICE],
+})
+class BuildServiceModule {}
+
+@Module({
+  imports: [
+    GraphQLGatewayModule.forRootAsync({
+      useFactory: async () => ({
+        gateway: {
+          serviceList: [
+            /* services */
+            // { name: 'users', url: 'http://user-service/graphql' },
+            // { name: 'posts', url: 'http://post-service/graphql' },
+          ],
+        },
+        server: {
+          context: ({ req }) => ({
+            jwt: req.headers.authorization,
+          }),
+        },
+      }),
+      imports: [BuildServiceModule],
+      inject: [GATEWAY_BUILD_SERVICE],
+    }),
+  ],
+})
+export class AppModule {}
